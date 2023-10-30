3301a0d1aa64e622be601199466f1e28c5cc03ee
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-10-30 16:46:13 +0300

    - инициализация проекта, настройка окружения

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.get('/', (req: Request, res: Response) => {
+	return res.json({
+		status: 'success',
+	})
+})
+
+app.listen(8000, () => console.log('Сервер запущен на порту: 8000'))
