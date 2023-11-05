b161acddab80dd86853827be9a6b93993fcb19a9
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-11-05 11:41:59 +0300

    - установка, настройка ( EsLint, Prettier ) - установил дополнительные пакеты зависимостей

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import express, { Request, Response } from 'express'
 const app = express()
 
 app.get('/', (req: Request, res: Response) => {
-	return res.json({
-		status: 'success',
-	})
+  return res.json({
+    status: 'success',
+  })
 })
 
 app.listen(8000, () => console.log('Сервер запущен на порту: 8000'))
