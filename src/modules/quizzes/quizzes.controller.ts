ab6b4e1f14b29a444ed03be4c1ff01d2a6bb5e49
HabArt1978 <105586654+HabArt1978@users.noreply.github.com>
2023-11-29 10:57:30 +0300

    - добавил информацию с количеством тестов в ответ клиенту

--- src/modules/quizzes/quizzes.controller.ts
+++ src/modules/quizzes/quizzes.controller.ts
@@ -2,8 +2,9 @@ import { Request, Response } from 'express'
 import { TQuizSchema, quizSchema } from './schemas/quiz.schema'
 import UserModel from '../../users/models/user.model'
 import TokenModel from '../auth/models/token.model'
-import QuizModel from './models/quiz.model'
+import QuizModel, { Quiz } from './models/quiz.model'
 import QuizListItemResponseDTO from './responses/quizListItemResponseDTO'
+import log from '../../utilities/logger'
 
 class QuizzesController {
   // ========================
@@ -79,15 +80,18 @@ class QuizzesController {
     //! Добавляем пагинацию
 
     // Если в параметре не задана задана страница, всегда отображаем 1-ю
-    const page = req.query || 0
+    const page = req.query.page || 0
     // Устанавливаем количество тестов отображаемых на одной странице
-    const quizzesPerPage = 3
+    const quizzesPerPage = 50
+    const quizzesTotalCount = await QuizModel.count()
+    const quizzesPageCount = Math.ceil(quizzesTotalCount / quizzesPerPage)
 
     const quizzes = await QuizModel.find()
+      .sort({ createdAt: -1 })
       .skip(+page * quizzesPerPage)
       .limit(quizzesPerPage)
 
-    const quizzesResponse = quizzes.map(quiz =>
+    const quizzesResponse = quizzes.map((quiz: Quiz) =>
       QuizListItemResponseDTO.fromModel(quiz),
     )
 
@@ -97,7 +101,7 @@ class QuizzesController {
         .json({ message: 'В базе данных пока нет тестов, создайте первый)' })
     }
 
-    return res.status(200).json(quizzesResponse)
+    return res.status(200).json({ quizzes: quizzesResponse, quizzesTotalCount })
   }
 }
 
